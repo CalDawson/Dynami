4c297c5674eb0fabf55e1ddbee6e44fc51b974d0
jesse kolb <jessekolb17@gmail.com>
2021-12-02 12:06:21 +1100

    new food actions

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -6,4 +6,30 @@ class MealsController < ApplicationController
     response = RestClient.get("https://api.calorieninjas.com/v1/nutrition?query=#{query}", headers={'X-Api-Key': ENV["FOOD_API_KEY"]})
     JSON.parse(response)['items'][0]
   end
+
+  def new
+    @meal = Meal.new
+    authorize @meal
+  end
+
+  def create
+    authorize Meal
+    meal = Meal.new
+  end
+
+  private
+
+  def meal_params
+    params.require(:meal).permit(:name)
+  end
 end
+
+  authorize Workout
+  workout = Workout.new(workout_params)
+  workout.user = current_user
+    if workout.save
+      redirect_to new_workout_workout_exercise_path(workout)
+    else
+      render :new
+    end
+  end
