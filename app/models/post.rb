b597aa777432a65cb0ce43dd37938240c0ac84f2
sholom sero <sholomsero123@gmail.com>
2021-12-02 12:17:27 +1100

    validations in models

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependant: :destroy
   has_one_attached :photo
+  validates :photo, attached: true
 end
--- test/controllers/meal_contents_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class MealContentsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
