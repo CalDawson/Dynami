782f5340a6613cbadaac383373b7910611d98dd9
Callum Dawson <cdawson198@gmail.com>
2021-12-05 13:46:53 +1100

    add4ed a like feature

--- app/javascript/likes/like.js
+++ app/javascript/likes/like.js
@@ -1,8 +1,14 @@
-like = document.querySelector(".like_subscribe")
+const like = document.querySelector(".like_subscribe")
+const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 
 
 like.addEventListener("click", function() {
   like.classList.toggle("liked");
-
-
+  const postId = like.getAttribute("data-post-id");
+  fetch(`/posts/${postId}/likes`, {
+    method: "POST",
+    headers: {
+      'X-CSRF-Token': token
+    },
+  });
 })
