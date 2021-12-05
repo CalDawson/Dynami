c8dfd1ef98421d22987161ece4ce3666474bae7b
Callum Dawson <cdawson198@gmail.com>
2021-12-05 14:15:08 +1100

    added like and unlike feature

--- app/javascript/likes/like.js
+++ app/javascript/likes/like.js
@@ -3,12 +3,21 @@ const token = document.querySelector('meta[name="csrf-token"]').getAttribute('co
 
 
 like.addEventListener("click", function() {
-  like.classList.toggle("liked");
   const postId = like.getAttribute("data-post-id");
-  fetch(`/posts/${postId}/likes`, {
-    method: "POST",
-    headers: {
-      'X-CSRF-Token': token
-    },
-  });
+  if (like.classList.contains("liked")) {
+    fetch(`/posts/${postId}/likes/0`, {
+      method: "DELETE",
+      headers: {
+        'X-CSRF-Token': token
+      },
+    });
+  } else {
+    fetch(`/posts/${postId}/likes`, {
+      method: "POST",
+      headers: {
+        'X-CSRF-Token': token
+      },
+    });
+  }
+  like.classList.toggle("liked");
 })
